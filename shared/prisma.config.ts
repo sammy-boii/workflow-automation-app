e1711809c11335fa9d656c623087579510f5ccf6
sammy-boii <neupanesamrajya2005@gmail.com>
2025-10-29 20:52:36 +0545

    fix: prisma config issue

--- shared/prisma.config.ts
+++ shared/prisma.config.ts
@@ -1,5 +1,5 @@
 import path from 'path'
-import { defineConfig } from '@prisma/config'
+import { defineConfig } from 'prisma/config'
 
 export default defineConfig({
   schema: path.resolve(__dirname, 'src/prisma/schema.prisma'),
